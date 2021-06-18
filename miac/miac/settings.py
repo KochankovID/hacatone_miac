aeb3e1cafaad5e5028f6e5626cfb23427173f0e0
ilyakochankov <fakafafakafa@mail.ru>
2021-06-19 02:24:51 +0300

    change access token lifetime to 1 day

--- miac/miac/settings.py
+++ miac/miac/settings.py
@@ -139,7 +139,7 @@ JWT_AUTH_COOKIE = "miac-auth"
 JWT_AUTH_REFRESH_COOKIE = "miac-refresh"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
