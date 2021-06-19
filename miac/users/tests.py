42502058ba355f9dc0aa532bec538297b89ec136
ilyakochankov <fakafafakafa@mail.ru>
2021-06-19 09:11:26 +0300

    add data generations

--- miac/users/tests.py
+++ miac/users/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+import rstr
+from users.models import User
+from django_any.models import any_model
+
+
+class GenerateUsers(TestCase):
+    def generate_Users(self):
+        username_gen = rstr.xeger('^\d{3}-\d{3}-\d{3}-\d{2}$')
+        user = any_model(User, username=username_gen)
